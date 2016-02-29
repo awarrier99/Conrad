3d6ae25c0974477dcafbd77ae787b1d0dc66703e
awarrier99 <warrierar@gmail.com>
2016-02-29 13:39:07 -0500

    Libs
    CMUSphinx Libs

--- src/ConradPrototype.java
+++ src/ConradPrototype.java
@@ -5,6 +5,5 @@ package com.warriermig.conrad;
 public class ConradPrototype {
 	public static void main(String[] args) {
 		System.out.println("Miguel is ugly af");
-		System.out.println("Jk, we all know he sexy af");
 	}
 }
