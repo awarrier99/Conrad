8cde10d025270045a295705d94c8ac824fe28cf4
awarrier99 <warrierar@gmail.com>
2016-03-01 14:51:13 -0500

    Updated Prototype

--- BARK/src/com/warriermig/conrad/Prototype.java
+++ BARK/src/com/warriermig/conrad/Prototype.java
@@ -8,11 +8,11 @@ import edu.cmu.sphinx.api.SpeechResult;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Prototype {       
                                      
     public static void main(String[] args) throws Exception {
-        System.out.println("hello");
         Configuration configuration = new Configuration();
 
         configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
@@ -20,27 +20,24 @@ public class Prototype {
         configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
         
         String buf = new String("");
-        Scanner fin = new Scanner(new File("res\\engdict.txt")).useDelimiter(",\\s*");
+        Scanner fin = new Scanner(new File("res\\engdict.txt"));
         ArrayList<String> words = new ArrayList<String>();
         String[] keywords = {"", "", "", ""};
         
         while (fin.hasNext()) {
-        	buf = fin.next();
+        	buf = fin.nextLine();
         	words.add(buf);
         }
         fin.close();
         
+        Random random = new Random();
         for (int x = 0; x < 3; x++) {
-        	System.out.println();
+        	keywords[x] = words.get(random.nextInt(words.size()));
         }
         
+        for (String s: keywords) {
+        	System.out.println(s);
+        }
         
-        
-        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
-
-        recognizer.startRecognition(true);
-        SpeechResult result = recognizer.getResult();
-        System.out.println(result.getHypothesis());
-        recognizer.stopRecognition();
     }
 }
