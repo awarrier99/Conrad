7c410612ec875be0337ed91a433a6f6bca8a8c4d
tiwanll <thisiswhataninjalookslike@gmail.com>
2016-03-04 02:41:57 -0500

    Commiting a lot of stuff

--- BARK/src/com/warriermig/conrad/GUI.java
+++ BARK/src/com/warriermig/conrad/GUI.java
@@ -11,6 +11,27 @@ import javafx.scene.effect.DropShadow;
 import javafx.animation.*;
 import javafx.util.Duration;
 
+//Prototype stuff:
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.warriermig.conrad.Prototype;
+
 public class GUI extends Application {
 	private Pane root;
 	private Label title;
@@ -18,30 +39,27 @@ public class GUI extends Application {
 	private double windowHeight;
 	private double windowWidth;
 	private DropShadow dropShadow;
+	private Label wordsDisp;
+	private ArrayList<String> words;
+	private String wordString;
+	
+	private static LiveSpeechRecognizer recognizer;
+	private static Configuration configuration;
 
 	public void setupRegion(){
-		root.getChildren().addAll(title, startButton);
+		root.getChildren().addAll(title, wordsDisp, startButton);
 	}
 	public void centerThings(){
 		title.setLayoutX((windowWidth-title.prefWidth(-1))/2);
 		title.setLayoutY((windowHeight-title.prefHeight(-1))/2-75);
+		wordsDisp.setLayoutX((windowWidth-wordsDisp.prefWidth(-1))/2);
+		wordsDisp.setLayoutY((windowHeight-wordsDisp.prefHeight(-1))/2-25);
 		startButton.setPrefWidth(60);
 		startButton.setPrefHeight(startButton.prefWidth(-1));
 		startButton.setLayoutX((windowWidth-startButton.prefWidth(-1))/2);
 		startButton.setLayoutY((windowHeight-startButton.prefHeight(-1))/2+50);
+		
 	}
-	// private void installEventHandler(final Node keyNode) {
-	// 	// handler for enter key press / release events, other keys are
-	// 	// handled by the parent (keyboard) node handler
-	// 	final EventHandler<MouseEvent> keyEventHandler =
-	// 	new EventHandler<MouseEvent>() {
-	// 		public void handle(final MouseEvent e) {
-				
-	// 		}
-	// 	};
-
-	// 	startButton.onMousePressed
-	// }
 	public void styleButton(){
 		dropShadow = new DropShadow();
 		dropShadow.setRadius(15.0);
@@ -80,8 +98,45 @@ public class GUI extends Application {
 		startButton.setOnMouseReleased(e -> {
 			anim.play();
 		});
+		startButton.setOnAction(e -> {
+			wordString = genWords();
+			dispWords(wordString);
+			Listener l = new Listener(recognizer);
+			FutureTask<String> ft1 = new FutureTask<String>(l);
+			String val = "";
+			ExecutorService executor = Executors.newFixedThreadPool(1);
+			
+			executor.execute(ft1);
+			while (true) {
+	            try {
+	                if(ft1.isDone()){
+	                    //shut down executor service
+	                    executor.shutdown();
+	                    break;
+	                }
+	                if(!ft1.isDone()){
+	                	val = ft1.get();
+	                }
+	            } catch (InterruptedException | ExecutionException ex) {
+	                ex.printStackTrace();
+	            }
+	        }
+//	        recognizer.startRecognition(true);
+			//SpeechResult result;
+//			result = recognizer.getResult();
+//	        while ((result = recognizer.getResult()) != null) {
+//	            System.out.println("You said: " + result.getHypothesis());
+//	        }
+			System.out.println("You said: " + val);
+//	        recognizer.stopRecognition();
+			validate(val);
+			centerThings();
+		});
 		
 		title = new Label("Hello!");
+		wordsDisp = new Label("");
+		wordsDisp.getStyleClass().add("text");
+		wordsDisp.setId("word");
 		title.getStyleClass().add("text");
 		title.setId("title");
 		
@@ -90,10 +145,66 @@ public class GUI extends Application {
 		// t.setFontSmoothingType(FontSmoothingType.LCD);
 		
 	}
+	
+	public void makeAllWords(){
+		String buf = new String("");
+        Scanner fin = null;
+		try {
+			fin = new Scanner(new File("res\\engdictedit.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+        words = new ArrayList<String>();
+        
+        while (fin.hasNext()) {
+        	buf = fin.nextLine();
+        	words.add(buf);
+        }
+        fin.close();
+	}
+	
+	public String genWords(){
+		String[] keywords = {"", "", ""};
+        Random random = new Random();
+        for (int x = 0; x < 3; x++) {
+        	keywords[x] = words.get(random.nextInt(words.size()));
+        }
+        
+        String keys = keywords[0]+" "+keywords[1]+" "+keywords[2];
+        return keys;
+	}
+	public void dispWords(String s){
+		title.setText("Say the following");
+		wordsDisp.setText(s);
+		centerThings();
+	}
+	
+	public void validate(String s){
+		if(s.equals(wordString)){
+			pass();
+		}else{
+			fail(s);
+		}
+	}
+	
+	public void pass(){
+		root.setStyle("-fx-background-color: #4CAF50;");
+		title.setText("Access Granted");
+		wordsDisp.setText("");
+	}
+	
+	public void fail(String s){
+		root.setStyle("-fx-background-color: #F44336;");
+		title.setText("Failed!");
+		wordsDisp.setText("");
+	}
 
 	@Override
 	public void start(Stage primary){
 		Scene s = new Scene(root, windowWidth, windowHeight);
+		makeAllWords();
+		wordString = genWords();
+		dispWords(wordString);
 		s.getStylesheets().add(getClass().getResource("gui.css").toString()); 
 		s.setFill(Color.web("#1A237E"));
 		primary.setScene(s);
@@ -103,6 +214,41 @@ public class GUI extends Application {
 	}
 
 	public static void main(String[] args){
+		configuration = new Configuration();
+
+        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration.setDictionaryPath("res\\2986.dic");
+        configuration.setLanguageModelPath("res\\2986.lm");
+        
+        try{
+			recognizer = new LiveSpeechRecognizer(configuration);
+		}catch(IOException exception){
+			System.out.println("Error");
+		}
+
+        
 		launch(args);
 	}
 }
+
+class Listener implements Callable<String> {
+	private static LiveSpeechRecognizer recognizer;
+	public Listener(LiveSpeechRecognizer recognizer){
+		this.recognizer = recognizer;
+	}
+    public String call(){
+    	recognizer.startRecognition(true);
+		SpeechResult result;
+		
+		result = recognizer.getResult();
+		String returnstr = "";
+        while (returnstr == "") {
+        	returnstr = result.getHypothesis();
+        }
+        System.out.println("In the thread it's " + returnstr);
+        recognizer.stopRecognition();
+        return returnstr;
+    }
+
+}
+
